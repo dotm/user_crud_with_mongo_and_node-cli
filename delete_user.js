6ec20e308eff5698c66561dc2f3352678b668e3b
dotm <desde.el.mar96@gmail.com>
2018-11-25 15:22:23 +0700

    finish implementing delete user by id function

---
+++ delete_user.js
@@ -0,0 +1,24 @@
+//Example CLI usage:
+//  node delete_user.js  ObjectID
+
+const connectToDB = require('./client.js').connectToDB
+const util = require('./util.js')
+const exitProcess = util.exitProcess
+const exitWithError = util.exitWithError
+const USER_COLLECTION = require('./collection_name_constants').USER_COLLECTION
+const ObjectID = require('mongodb').ObjectID;
+
+const deleteUser_byID = (db, id) => {
+    return db.collection(USER_COLLECTION).deleteOne(
+        {_id: ObjectID(id)},
+    )
+}
+
+let argv = require('minimist')(process.argv.slice(2));
+let user_id = argv._[0]
+
+connectToDB
+    .then((db)=> deleteUser_byID(db, user_id))
+    .then(console.log)
+    .then(exitProcess)
+    .catch(exitWithError)
